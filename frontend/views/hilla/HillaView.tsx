e8333c2ccfcec67ac470e98f771f920d9ba80acb
Mikhail Shabarov <mikhail@vaadin.com>
2024-01-05 12:28:36 +0200

    Adding Flow component to react

--- frontend/views/hilla/HillaView.tsx
+++ frontend/views/hilla/HillaView.tsx
@@ -2,14 +2,37 @@ import { Button } from "@hilla/react-components/Button.js";
 import { Notification } from "@hilla/react-components/Notification.js";
 import { TextField } from "@hilla/react-components/TextField.js";
 import { HelloEndpoint } from "Frontend/generated/endpoints.js";
-import { useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import {VerticalLayout} from "@hilla/react-components/VerticalLayout.js";
+//@ts-ignore
+import {LoginForm} from "Frontend/generated/flow/web-components/login-form.js";
 
 export default function HillaView() {
   const [name, setName] = useState("");
 
+  //@ts-ignore
+  const ref = useRef(this);
+
+    useEffect(() => {
+        const el = document.createElement('login-form') as LoginForm;
+        el.userlbl = "Your username";
+        el.pwdlbl = "Your password";
+        const layout = ref.current.querySelectorAll('vaadin-vertical-layout')[0];
+
+        if (layout) {
+            layout.appendChild(el);
+        }
+
+        return () => {
+            // Cleanup when the component unmounts
+            if (layout && el.parentNode === layout) {
+                layout.removeChild(el);
+            }
+        };
+    }, []);
+
   return (
-    <>
+    <div ref={ref}>
         <VerticalLayout className={'centered-content'}>
             <h3>Hilla View</h3>
             <TextField
@@ -25,6 +48,6 @@ export default function HillaView() {
                 }}
             > Say hello </Button>
         </VerticalLayout>
-    </>
+    </div>
   );
 }
