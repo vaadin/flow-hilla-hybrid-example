4c4b8cb9cc5fb955a8dfe01e43ce873d70e08174
Manolo Carrasco <manolo@vaadin.com>
2024-01-15 21:41:33 +0100

    feat: make hilla-view have the same behavior than flow-view

--- src/main/java/org/vaadin/example/endpoints/HelloEndpoint.java
+++ src/main/java/org/vaadin/example/endpoints/HelloEndpoint.java
@@ -1,5 +1,7 @@
 package org.vaadin.example.endpoints;
 
+import org.vaadin.example.GreetService;
+
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 import dev.hilla.Nonnull;
@@ -8,12 +10,10 @@ import dev.hilla.Nonnull;
 @AnonymousAllowed
 public class HelloEndpoint {
 
+    GreetService service = new GreetService();
+
     @Nonnull
     public String sayHello(@Nonnull String name) {
-        if (name.isEmpty()) {
-            return "Hello stranger";
-        } else {
-            return "Hello " + name;
-        }
+        return service.greet(name);
     }
 }
