26c5320342a4a01b0651e10612ba69135527b3e9
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2024-02-07 19:43:04 -0800

    Cleaning up About view to make it look the same with the rest of the views

--- frontend/views/about/AboutView.tsx
+++ frontend/views/about/AboutView.tsx
@@ -1,12 +1,17 @@
+import { VerticalLayout } from "@vaadin/react-components/VerticalLayout.js";
+
+
 export default function AboutView() {
 
     return (
-        <>
-            <p>This is a simplified example on how to mix:</p>
-            <ul>
-                <li>Hilla with React</li>
-                <li>Flow in pure Java</li>
-            </ul>
-        </>
+        <VerticalLayout theme="padding spacing">
+            <h3>About View</h3>
+            <p>This is a simplified example on how to mix:
+                <ul>
+                    <li>Hilla with React</li>
+                    <li>Flow in pure Java</li>
+                </ul>
+            </p>
+        </VerticalLayout>
     );
 }
