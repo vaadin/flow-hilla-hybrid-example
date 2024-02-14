eba8980b74ac1c6319eb96e3898b9f24fc3b4163
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2024-02-14 14:30:04 +0200

    Minor cleanup

--- frontend/views/about/AboutView.tsx
+++ frontend/views/about/AboutView.tsx
@@ -4,14 +4,12 @@ import { VerticalLayout } from "@vaadin/react-components/VerticalLayout.js";
 export default function AboutView() {
 
     return (
-        <VerticalLayout theme="padding spacing">
-            <h3>About View</h3>
-            <p>This is a simplified example on how to mix:
-                <ul>
-                    <li>Hilla with React</li>
-                    <li>Flow in pure Java</li>
-                </ul>
-            </p>
+        <VerticalLayout theme="padding">
+            <p>This project is a simplified example on how to mix:</p>
+            <ul>
+                <li>Hilla with React</li>
+                <li>Flow in pure Java</li>
+            </ul>
         </VerticalLayout>
     );
 }
