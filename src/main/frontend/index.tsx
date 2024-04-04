1db18849e81b730f639f1b57d1c99ed4c072adad
Mikhail Shabarov <mikhail@vaadin.com>
2024-04-04 15:37:52 +0300

    update with the latest changes in Hilla

--- src/main/frontend/index.tsx
+++ src/main/frontend/index.tsx
@@ -2,7 +2,7 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider} from "./auth";
-import router from 'Frontend/routes.js';
+import { router } from 'Frontend/routes.js';
 
 function App() {
     return (
