f41ffe72e2218c4e66136852a865ef286260b7ec
Mikhail Shabarov <mikhail@vaadin.com>
2024-06-18 10:55:05 +0300

    test: Test usage statistics

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,20 +1,31 @@
 import { VerticalLayout } from "@vaadin/react-components";
 import type { ViewConfig } from "@vaadin/hilla-file-router/types.js";
+import {reactElement} from "Frontend/generated/flow/Flow";
 
 export const config: ViewConfig = {
-    menu: {
-        title: "Public page",
-        icon: 'vaadin:group'
-    }
+  menu: {
+    title: "Public page",
+    icon: 'vaadin:group'
+  }
 };
 
-/**
- * Hilla view that is available publicly.
- */
+type MyProperties = {
+  greeting: string
+}
+
+function MyFlowComponent(props: MyProperties) {
+  return reactElement("my-flow-component", { greeting: props.greeting },
+    () => {
+      document.getElementById("loading")?.remove();
+    }, (err: any) => { console.error(err); });
+}
+
 export default function Public() {
     return (
-        <VerticalLayout theme="padding">
-            <p>This is a Hilla view available publicly.</p>
-        </VerticalLayout>
+      <VerticalLayout theme="padding">
+        <p>This is a Hilla view</p>
+        <div id={"loading"}>Loading...</div>
+        <MyFlowComponent greeting={'Hello from Flow component in Hilla!'}/>
+      </VerticalLayout>
     );
 }
--- src/main/java/org/vaadin/example/FlowView.java
+++ src/main/java/org/vaadin/example/FlowView.java
@@ -2,12 +2,18 @@ package org.vaadin.example;
 
 import jakarta.annotation.security.RolesAllowed;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.html.H3;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeLeaveEvent;
+import com.vaadin.flow.router.BeforeLeaveObserver;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,11 +21,23 @@ import com.vaadin.flow.router.Route;
 /**
  * Flow view that is available only for users with role 'ADMIN'
  */
-@RolesAllowed("ADMIN")
+@RolesAllowed("USER")
 @Menu(title = "Flow admin", icon = "vaadin:tools")
 @Route("flow")
 @PageTitle("Flow Admin page")
 public class FlowView extends VerticalLayout {
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        super.onDetach(detachEvent);
+        Notification.show("OnDetach");
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        Notification.show("OnAttach");
+    }
 
     public FlowView(GreetService service) {
 
@@ -33,6 +51,8 @@ public class FlowView extends VerticalLayout {
         button.setId("say-hello");
 
         add(new H3("Flow Admin View"), textField, button);
+
+        add(new PickerComponent());
     }
 
 }
