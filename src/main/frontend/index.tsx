7c18083d32f39c4c950c02ba5cfce1852509f794
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-03-19 13:45:10 +0200

    update vaadin 24.7.0 (#99)

--- src/main/frontend/index.tsx
+++ src/main/frontend/index.tsx
@@ -4,13 +4,12 @@
 
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 import { router } from 'Frontend/generated/routes';
 import { AuthProvider } from './auth';
 
 function App() {
     return <AuthProvider>
-        <RouterProvider router={router} future={{ v7_startTransition: true }} />
     </AuthProvider>;
 }
 
