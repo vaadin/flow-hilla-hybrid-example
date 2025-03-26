1c9d2163ca96f3a40dd307dbc3b3e7f416401bc8
Manuel Carrasco Moñino <manolo@vaadin.com>
2025-03-26 13:54:16 +0100

    test: fix tests and update test depencencies (#100)
    * test: fix playwright test specs
    
    * adjust maven phase for npm install
    
    * Installing playwright is not necessary from maven
    
    * fix build actions for ACT
    
    * install packages
    
    * update spring-boot
    
    * chore: update vite to avoid vulnerability warning
    
    * Add AfterEach method to close page and avoid exception in console
    
    * fix flaky

--- src/test/frontend/views/about.spec.ts
+++ src/test/frontend/views/about.spec.ts
@@ -9,11 +9,11 @@ test.beforeEach(async ({ page }) => {
   await page.getByLabel('Username').fill("user");
   await page.getByLabel('Password', { exact: true }).fill("user");
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForURL(new RegExp(`${getUrl()}(\\?continue)?`));
-  await page.waitForTimeout(300);
+  await page.waitForURL(getUrl());
 });
 
 test('should display a view for authenticated users', async ({ page }) => {
+  await page.waitForSelector('p');
   await expect(await page.locator('p')).toContainText('This is a Hilla view available for authenticated users.');
 });
 
--- src/test/frontend/views/index.spec.ts
+++ src/test/frontend/views/index.spec.ts
@@ -6,11 +6,11 @@ function getUrl() {
 
 test.beforeEach(async ({ page }) => {
   await page.goto(getUrl());
-  await page.waitForURL(new RegExp(`${getUrl()}(\\?continue)?`));
-  await page.waitForTimeout(300);
+  await page.waitForURL(getUrl());
 });
 
 test('showld display a public message ', async ({ page }) => {
+  await page.waitForSelector('p');
   await expect(await page.locator('p').first()).toBeVisible();
   await expect(page.locator('p')).toContainText('This is a Hilla view available publicly.');
 });
