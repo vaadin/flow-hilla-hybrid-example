a98fc03676f8b3b222ba893c6197d610277281b1
Manolo Carrasco <manolo@vaadin.com>
2025-03-27 15:08:11 +0100

    Make tests pass in both production and dev modes

--- src/test/frontend/views/index.spec.ts
+++ src/test/frontend/views/index.spec.ts
@@ -6,7 +6,7 @@ function getUrl() {
 
 test.beforeEach(async ({ page }) => {
   await page.goto(getUrl());
-  await page.waitForURL(getUrl());
+  await page.waitForURL(new RegExp(getUrl() + '(.continue)?'));
 });
 
 test('showld display a public message ', async ({ page }) => {
